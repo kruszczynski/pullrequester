479a5ae909cef6323c7b44a17fb5cfe861505ca5
Bartek Kruszczyński <bartek@shedul.com>
2018-02-13 00:06:34 +0100

    Add index route

--- app/server.rb
+++ app/server.rb
@@ -9,6 +9,11 @@ module PullRequester
   class Server < Sinatra::Base
     set :storage, Storage.new(ENV.fetch('DATABASE_URL'))
 
+    get '/' do
+      @index_message = 'Welcome to Pull Requester'
+      erb :index
+    end
+
     post '/webhooks/:uuid' do
       # we're only interested in pull request events
       return 200 unless request.env['HTTP_X_GITHUB_EVENT'] == 'pull_request'
